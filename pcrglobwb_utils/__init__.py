ab07411216ff1318f8426bf619d12062e73d0195
Hoch <J.M.Hoch@uu.nl>
2020-03-19 18:30:40 +0100

    restructuring files and folders and first trial and error with click

--- pcrglobwb_utils/__init__.py
+++ pcrglobwb_utils/__init__.py
@@ -1,5 +1,11 @@
 """Top-level package for pcrglobwb_utils."""
 
+from . import timeseries
+from . import get_grdc_info
+from . import utils
+from . import plotting
+from . import pcrglobwb_utils  
+
 __author__ = """Jannis M. Hoch"""
 __email__ = 'j.m.hoch@uu.nl'
 __version__ = '0.1.0'
--- pcrglobwb_utils/cli.py
+++ pcrglobwb_utils/cli.py
@@ -4,11 +4,22 @@ import click
 
 
 @click.command()
-def main(args=None):
+@click.argument('cfg',)
+
+# @click.option('-s', '--single-file', default='', help='only single nc-file to be analysed')
+# @click.option('-m', '--multi-file', default='', help='multiple nc-files to be analysed')
+
+def main(cfg):
     """Console script for pcrglobwb_utils."""
     click.echo("Replace this message by putting your code into "
                "pcrglobwb_utils.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
+
+    click.echo("Hello main, {}".format(cfg))
+
+    test = cfg
+    print(str(test))
+
     return 0
 
 
