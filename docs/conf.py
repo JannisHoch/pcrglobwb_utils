7e8e085ffeb9e42e7bdf133b4be733969e55cc6e
Hoch <J.M.Hoch@uu.nl>
2020-03-23 15:04:56 +0100

    changes html theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,17 +16,17 @@ import os
 import sys
 from mock import Mock as MagicMock
 
-class Mock(MagicMock):
-    __all__ = []
-    __version__ = "1.6"
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
+# class Mock(MagicMock):
+#     __all__ = []
+#     __version__ = "1.6"
+#     @classmethod
+#     def __getattr__(cls, name):
+#             return Mock()
 
-# Mock modules so that we can build on readthedocs.org
-MOCK_MODULES = ['xarray', 'rasterio']
+# # Mock modules so that we can build on readthedocs.org
+# MOCK_MODULES = ['xarray', 'rasterio']
 
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+# sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
@@ -112,7 +112,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
