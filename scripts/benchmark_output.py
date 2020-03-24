036e345c6a497096393ef89e5e05dccb7868896b
Hoch <J.M.Hoch@uu.nl>
2020-03-24 11:04:11 +0100

    replacing row/col input in benchmark script with lon/lat

--- scripts/benchmark_output.py
+++ scripts/benchmark_output.py
@@ -11,8 +11,8 @@ import os, sys
 # parsing arguments and checking whether provided paths are absolute or relative
 ncfiles_list = sys.argv[1:-3] # list of nc-files to be evaluated
 
-row = int(sys.argv[-3])
-col = int(sys.argv[-2])
+lon = float(sys.argv[-3]) #lon/lat
+lat = float(sys.argv[-2])
 
 out_dir = str(sys.argv[-1]) # path to output_dir
 if os.path.isabs(out_dir):
@@ -45,6 +45,9 @@ for i, file in enumerate(ncfiles_list):
 
     print('reading from nc-file', os.path.abspath(nc_file))
     print('')
+
+    # get row/col from lon/lat
+    row, col = pcrglobwb_utils.utils.find_indices_from_coords(nc_file, lon, lat)
     
     # extract information at a certain row/col
     q_sim = pcrglobwb_utils.timeseries.read_nc_file_at_indices(nc_file, 
--- scripts/run_example_benchmark.sh
+++ scripts/run_example_benchmark.sh
@@ -1,8 +1,8 @@
 # paths to files and dirs as well as settings
 nc_file='..\examples\example_data\DUMMY_discharge_dailyTot_output.nc'
-row=17
-col=50
+lon=-55.511100
+lat=-1.947200
 out_dir='..\examples\_benchmark_out\'
 
 # execute script
-python benchmark_output.py $nc_file $nc_file $nc_file $row $col $out_dir
+python benchmark_output.py $nc_file $nc_file $nc_file $lon $lat $out_dir
