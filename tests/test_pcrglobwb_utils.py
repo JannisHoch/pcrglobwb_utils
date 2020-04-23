64e6eeb77ab1e85bc9833195ed7f34a11b6ac624
Hoch <J.M.Hoch@uu.nl>
2020-04-23 14:47:02 +0200

    added a test for daily2monthly

--- tests/test_pcrglobwb_utils.py
+++ tests/test_pcrglobwb_utils.py
@@ -4,10 +4,8 @@
 
 import pytest
 
-from click.testing import CliRunner
-
-from pcrglobwb_utils import pcrglobwb_utils
-from pcrglobwb_utils import cli
+import pcrglobwb_utils
+import pandas as pd
 
 
 @pytest.fixture
@@ -26,12 +24,29 @@ def test_content(response):
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'pcrglobwb_utils.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+# def test_command_line_interface():
+#     """Test the CLI."""
+#     runner = CliRunner()
+#     result = runner.invoke(cli.main)
+#     assert result.exit_code == 0
+#     assert 'pcrglobwb_utils.cli.main' in result.output
+#     help_result = runner.invoke(cli.main, ['--help'])
+#     assert help_result.exit_code == 0
+#     assert '--help  Show this message and exit.' in help_result.output
+
+def test_daily2monthly():
+
+    df = '../examples/example_data/Obidos_data.csv'
+
+    csv_obj = pcrglobwb_utils.obs_data.other_data(df)
+
+    df_CSV = csv_obj.get_values_from_csv(t_col='YYYY-MM-DD', 
+                                         v_col='Calculated', 
+                                         plot=True,
+                                         datetime_format='%d-%m-%Y')
+
+    df_test = pcrglobwb_utils.time_funcs.daily2monthly(df_CSV)
+
+    print(df_test.dt)
+
+
