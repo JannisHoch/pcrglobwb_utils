2d02a855307d568a500b5636e355b9f8f3a93374
Hoch <J.M.Hoch@uu.nl>
2020-05-18 16:34:24 +0200

    fixed click evaluate_output.py

--- scripts/evaluate_output.py
+++ scripts/evaluate_output.py
@@ -35,9 +35,8 @@ def main(ncf, out_dir, grdc_file=None, csv_file=None, latitude=None, longitude=N
     print(datetime.datetime.now())
     print('')
 
-    # parsing arguments and checking whether provided paths are absolute or relative
-    ncfiles_list = ncf
-
+    ## OBSERVED DATA ##
+    # getting and checking files and settings for observed data (GRDC or CSV)
     if grdc_file is not None:
         obs_file = grdc_file
     elif csv_file is not None:
@@ -62,25 +61,12 @@ def main(ncf, out_dir, grdc_file=None, csv_file=None, latitude=None, longitude=N
     if csv_file is not None:
         obs_data = pcrglobwb_utils.obs_data.other_data(obs_file)
 
-    out_dir = out_dir # path to output_dir
-
-    # check whether path provided is absolute or relative
-    if os.path.isabs(out_dir):
-        pass
-    else:
-        out_dir = os.path.join(os.getcwd(), out_dir)
-
-    # check whether out_dir already exists, if not make a new folder
-    if os.path.exists(out_dir):
-        pass
-    else:
-        os.mkdir(out_dir)
-
     # provding plot names 
     #TODO: make this more interactive and less hard-coded
     new_var_name_obs = 'Qobs [m3/s]'
     new_var_name_sim = 'Qsim [m3/s]'
 
+    # get observed data from GRDC file
     if grdc_file is not None:
 
         # get GRDC station properties
@@ -93,44 +79,57 @@ def main(ncf, out_dir, grdc_file=None, csv_file=None, latitude=None, longitude=N
         df_obs = obs_data.get_grdc_station_values(var_name=new_var_name_obs,
                                                 plot=False)
 
+    # get observed data from CSV file
     if csv_file is not None:
 
         df_obs = obs_data.get_values_from_csv()
 
+    ## OUTPUT DIRECTORY ##
+    # check whether output path provided is absolute or relative
+    if os.path.isabs(out_dir):
+        pass
+    else:
+        out_dir = os.path.join(os.getcwd(), out_dir)
+
+    # check whether output path already exists, if not make a new folder
+    if os.path.exists(out_dir):
+        pass
+    else:
+        os.mkdir(out_dir)
+
+    ## NC FILE ##
     #NOTE: tested with 1 file so far only
-    for file in ncfiles_list:
+    if os.path.isabs(ncf):
+        pass
+    else:
+        ncf = os.path.join(os.getcwd(), ncf)
 
-        if os.path.isabs(file):
-            pass
-        else:
-            nc_file = os.path.join(os.getcwd(), file)
+    print('reading from nc-file', os.path.abspath(ncf))
+    print('')
 
-        print('reading from nc-file', os.path.abspath(nc_file))
-        print('')
+    row, col = pcrglobwb_utils.utils.find_indices_from_coords(ncf, 
+                                                            grdc_props['longitude'], 
+                                                            grdc_props['latitude'])
 
-        row, col = pcrglobwb_utils.utils.find_indices_from_coords(nc_file, 
-                                                                grdc_props['longitude'], 
-                                                                grdc_props['latitude'])
+    pcr_data = pcrglobwb_utils.sim_data.from_nc(ncf)
 
-        pcr_data = pcrglobwb_utils.sim_data.from_nc(nc_file)
+    q_sim = pcr_data.read_values_at_indices(row,
+                                            col,
+                                            plot_var_name=new_var_name_sim)
 
-        q_sim = pcr_data.read_values_at_indices(row,
-                                                col,
-                                                plot_var_name=new_var_name_sim)
-
-        df_eval, eval_dic = pcr_data.validate_results(df_grdc,
-                                                    out_dir,
-                                                    new_var_name_obs,
-                                                    new_var_name_sim,
-                                                    plot=False,
-                                                    save_fig=True)
-        
-        print('head of evaluated dataframe columns')
-        print(df_eval.head())
-        print('')
-        
-        print('timeseries validation results are:')
-        print(eval_dic)
+    df_eval, eval_dic = pcr_data.validate_results(df_obs,
+                                                out_dir,
+                                                new_var_name_obs,
+                                                new_var_name_sim,
+                                                plot=False,
+                                                save_fig=True)
+    
+    print('head of evaluated dataframe columns')
+    print(df_eval.head())
+    print('')
+    
+    print('timeseries validation results are:')
+    print(eval_dic)
 
     df_eval.to_csv(os.path.join(out_dir, 'dataframe.csv'), sep=';')
 
