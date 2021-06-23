49aa32f05beca912ce8c25f5293b1ce66a8f659a
JannisHoch <j.m.hoch@uu.nl>
2021-06-23 12:23:41 +0200

    only GRDC tested (for now)

--- scripts/test_discharge_validation.sh
+++ scripts/test_discharge_validation.sh
@@ -2,19 +2,7 @@
 path_to_script='.'
 nc_file='..\examples\example_data\DUMMY_discharge_dailyTot_output.nc'
 grdc_file='..\examples\example_data\3629000_Obidos.day'
-out_dir='validation_out_with_grdc'
+out_dir='eval_GRDC'
 
 # execute script
-python $path_to_script/evaluate_output.py $nc_file $out_dir -g $grdc_file
-
-# paths to files and dirs
-path_to_script='.'
-nc_file='..\examples\example_data\DUMMY_discharge_dailyTot_output.nc'
-grdc_file='..\examples\example_data\3629000_Obidos.day'
-excel_file='..\examples\example_data\Obidos_data.xlsx'
-latitude=-1.9472
-longitude=-55.5111
-out_dir='validation_out_with_excel'
-
-# execute script
-python $path_to_script/evaluate_output.py $nc_file $out_dir -e $excel_file -lat $latitude -lon $longitude
+python $path_to_script/evaluate_timeseries.py $nc_file $out_dir -g $grdc_file
