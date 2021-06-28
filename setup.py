21c770742c137eb7f4b8d91b79076e1978e6ccc8
Jannis <j.m.hoch@uu.nl>
2021-06-28 13:45:51 +0200

    Bump version: 0.2.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://pcrglobwb-utils.readthedocs.io/',
-    version='0.2.0',
+    version='version='0.2.0'',
     zip_safe=False,
 )
