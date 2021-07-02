da0080b2a55a10b43507b027f071cb929f6ac902
Jannis <j.m.hoch@uu.nl>
2021-07-02 15:01:03 +0200

    0.2.4 beta release

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://pcrglobwb-utils.readthedocs.io/',
-    version='0.2.3',
+    version='0.2.4b',
     zip_safe=False,
 )
