c464bcb47833be3dc69d24e29c3e504d3f826c2a
JannisHoch <j.m.hoch@uu.nl>
2021-10-22 09:13:12 +0200

    next beta version

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://pcrglobwb-utils.readthedocs.io/',
-    version='0.2.5b1',
+    version='0.2.5b2',
     zip_safe=False,
 )
