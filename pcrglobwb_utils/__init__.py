693ca33bcba28c6f715dfbd97cdf8517e4152e89
Jannis <j.m.hoch@uu.nl>
2021-11-24 19:44:12 +0100

    Bump version: 0.3.2 → 0.3.3

--- pcrglobwb_utils/__init__.py
+++ pcrglobwb_utils/__init__.py
@@ -12,4 +12,4 @@ from . import ensembles
 __author__ = """Jannis M. Hoch, Niko Wanders"""
 __institute__= """Utrecht University"""
 __email__ = 'j.m.hoch@uu.nl'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
