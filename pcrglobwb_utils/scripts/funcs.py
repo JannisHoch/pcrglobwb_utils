e03d92897794b76a524451453b305c33eaceb9c8
Jannis <j.m.hoch@uu.nl>
2021-11-25 10:51:41 +0100

    bugfix inception

--- pcrglobwb_utils/scripts/funcs.py
+++ pcrglobwb_utils/scripts/funcs.py
@@ -69,7 +69,7 @@ def evaluate_polygons(ID, ply_id, extent_gdf, obs_d, sim_d, obs_var_name, sim_va
 
             click.echo('INFO -- no mask for observed data found for ID {}, pass.'.format(ID))
             obs_df = pd.DataFrame(data=[], index=obs_idx, columns=[obs_var_name])
-            sim_df = pd.DataFrame(data=[], index=obs_idx, columns=[obs_var_name])
+            sim_df = pd.DataFrame(data=[], index=obs_idx, columns=[sim_var_name])
             final_df = pd.concat([obs_df, sim_df], axis=1).dropna()
 
         # if no data is found for poly ID (i.e. an empty df is returned) for simulation data, then create empty dummy df for evaluation later
@@ -77,7 +77,7 @@ def evaluate_polygons(ID, ply_id, extent_gdf, obs_d, sim_d, obs_var_name, sim_va
 
             click.echo('INFO -- no mask for simulated data found for ID {}, pass.'.format(ID))
             obs_df = pd.DataFrame(data=[], index=obs_idx, columns=[obs_var_name])
-            sim_df = pd.DataFrame(data=[], index=obs_idx, columns=[obs_var_name])
+            sim_df = pd.DataFrame(data=[], index=obs_idx, columns=[sim_var_name])
             final_df = pd.concat([obs_df, sim_df], axis=1).dropna()
 
     # if no file with pickled list of pickled masks is provided, clip here on-the-fly
