7bf493720acff4a460a7b0b433de962e220f4695
JannisHoch <j.hoch@fathom.global>
2023-01-30 13:55:56 +0100

    revamped and aligned functions
    also upgraded docstring and typecast

--- pcrglobwb_utils/time_funcs.py
+++ pcrglobwb_utils/time_funcs.py
@@ -4,48 +4,22 @@
 import pandas as pd
 import click
 
-
-def calc_montly_avg(df_in, var_name=None):
-    """Calculates the monthly averages of a timeseries.
-
-    Arguments:
-        df_in (dataframe): pandas dataframe containing timeseries
-
-    Keyword Arguments:
-        var_name (str): header of column in df_in from which monthly averages are to be computed (default: None)
-
-    Returns:
-        dataframe: pandas dataframe containing timeseries with monthly averages
-    """ 
-
-    # if variable name is not None, then pick values from specified column
-    if var_name != None:
-        df = df_in[var_name]
-    # else, just use the dataframe as is
-    else:
-        df = df_in
-    
-    # group values by month and then calculate mean
-    df_out = df.groupby(df.index.month).mean()
-    
-    return df_out
-
-
-def resample_to_month(df, stat_func='mean'):
-    """[summary]
+def resample_to_month(df: pd.DataFrame, stat_func='mean', suffix=None) -> pd.DataFrame:
+    """Resamples a timeseries at sub-monthly time step to monthly values. 
+    A range of monthly statistics can be chosen.
+    If desired, the column name of the returned dataframe can contain a suffix for better distinguishment.
+    By default, column names are unaltered.
 
     Args:
-        df ([type]): [description]
-        stat_func (str, optional): [description]. Defaults to 'mean'.
-
-    Raises:
-        ValueError: [description]
+        df (pd.DataFrame): dataframe containing timeseries. Note, only tested with dataframes containing one column.
+        stat_func (str, optional): Statistical method to be used. Either 'mean', 'max', 'min' or 'sum'. Defaults to 'mean'.
+        suffix (str, optional): Suffix to be added to column of returned dataframe. Defaults to None.
 
     Returns:
-        [type]: [description]
-    """    
+        pd.DataFrame: dataframe containing resampled timeseries.
+    """
 
-    click.echo('INFO -- resampling data to monthly time scale.')
+    click.echo('INFO -- resampling data to monthly {}.'.format(stat_func))
     if stat_func == 'mean':
         df = df.resample('M', convention='start').mean()
     elif stat_func == 'max':
@@ -55,23 +29,27 @@ def resample_to_month(df, stat_func='mean'):
     elif stat_func == 'sum':
         df = df.resample('M', convention='start').sum()
     else:
-        raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+        raise ValueError('no supported statistical function provided - choose between mean, max, min or sum')
+
+    if suffix != None:
+        df = df.add_suffix(suffix)
 
     return df
 
-def resample_to_annual(df, stat_func='mean'):
-    """[summary]
+def resample_to_annual(df: pd.DataFrame, stat_func='mean', suffix=None) -> pd.DataFrame:
+    """Resamples a timeseries at sub-annual time step to annual values. 
+    A range of annual statistics can be chosen.
+    If desired, the column name of the returned dataframe can contain a suffix for better distinguishment.
+    By default, column names are unaltered.
 
     Args:
-        df ([type]): [description]
-        stat_func (str, optional): [description]. Defaults to 'mean'.
-
-    Raises:
-        ValueError: [description]
+        df (pd.DataFrame): dataframe containing timeseries. Note, only tested with dataframes containing one column.
+        stat_func (str, optional): Statistical method to be used. Either 'mean', 'max', 'min' or 'sum'. Defaults to 'mean'.
+        suffix (str, optional): Suffix to be added to column of returned dataframe. Defaults to None.
 
     Returns:
-        [type]: [description]
-    """    
+        pd.DataFrame: dataframe containing resampled timeseries.
+    """
 
     click.echo('INFO -- resampling data to yearly time scale.')
     if stat_func == 'mean':
@@ -83,31 +61,32 @@ def resample_to_annual(df, stat_func='mean'):
     elif stat_func == 'sum':
         df = df.resample('Y', convention='start').sum()
     else:
-        raise ValueError('no supported statistical function provided - choose between mean, max, and min')
+        raise ValueError('no supported statistical function provided - choose between mean, max, min or sum')
+
+    if suffix != None:
+        df = df.add_suffix(suffix)
 
     return df
 
-def resample_ts(df_obs, df_sim, time_scale):
-    """[summary]
+def calc_monthly_climatology(df_in: pd.DataFrame, col_name=None) -> pd.DataFrame:
+    """Calculates the climatological mean of each month across a timeseries at sub-monthly timestep.
 
     Args:
-        df_obs ([type]): [description]
-        df_sim ([type]): [description]
-        time_scale ([type]): [description]
+        df_in (pd.DataFrame): dataframe containing timeseries at sub-monthly timestep.
+        col_name (str, optional): name of column to be considered only. Defaults to None.
 
     Returns:
-        [type]: [description]
-    """    
+        pd.DataFrame: dataframe containing mean of each month.
+    """
 
-    # resample if specified to other time scales
-    if time_scale == 'month':
-        df_obs = resample_to_month(df_obs, stat_func='mean')
-        df_sim = resample_to_month(df_sim, stat_func='mean')
-    elif time_scale == 'year':
-        df_obs = resample_to_annual(df_obs, stat_func='mean')
-        df_sim = resample_to_annual(df_sim, stat_func='mean')
+    # if variable name is not None, then pick values from specified column
+    if col_name != None:
+        df = df_in[col_name]
+    # else, just use the dataframe as is
     else:
-        df_obs = df_obs
-        df_sim = df_sim    
-
-    return df_obs, df_sim
+        df = df_in
+    
+    # group values by month and then calculate mean
+    df_out = df.groupby(df.index.month).mean()
+    
+    return df_out
