541af7bd2230ed9b8e4f19836565eeca324f8d58
JannisHoch <j.hoch@fathom.global>
2023-02-02 10:31:02 +0100

    using more recent sphinx version

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ from datetime import datetime
 sys.path.insert(0, os.path.abspath('../pcrglobwb_utils'))
 # sys.path.insert(0, os.path.abspath('.'))
 
-import pcrglobwb_utils
+# import pcrglobwb_utils
 
 # -- General configuration ---------------------------------------------
 
@@ -106,7 +106,7 @@ release = str(1)
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
